42e2a08c8e89bd881c7fbbe7801e340349ba5c6f
Shobhit150 <vshobhit150@gmail.com>
2025-05-08 15:50:44 +0530

    critical bug

--- models/User.js
+++ models/User.js
@@ -4,7 +4,7 @@ const userSchema = new mongoose.Schema({
     _id : {type:String, required: true},
     name : {type:String, required: true},
     email : {type:String, required: true, unique :true},
-    image : {type:String, required: true},
+    imageUrl : {type:String, required: true},
     cartItems : {type:Object, default : {}},
 }, { minimize : false })
 
